ca6efb077f7413193b10176a6f23f6e65b28c466
Juvid <jutewei@qq.com>
2018-04-02 15:26:26 +0800

    Initial Commit

---
+++ JuCycleScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JuCycleScrollView
+//
+//  Created by Juvid on 2018/4/2.
+//  Copyright © 2018年 Juvid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JuCycleScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JuCycleScrollView
+//
+//  Created by Juvid on 2018/4/2.
+//  Copyright © 2018年 Juvid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
