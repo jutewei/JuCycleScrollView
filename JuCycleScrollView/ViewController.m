8cfbd0a36740603f56e653f2b315a4361e8ddd93
Juvid <jutewei@qq.com>
2018-04-03 15:04:01 +0800

    代码整理

--- JuCycleScrollView/ViewController.m
+++ JuCycleScrollView/ViewController.m
@@ -22,13 +22,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     ju_cycleScrollView.juItemW=[[UIScreen mainScreen] bounds].size.width-80;
+    [ju_cycleScrollView juSetScrollItem:@[@"1.jpg",@"2.jpeg",@"3.jpg",@"4.jpeg"]];
+    [ju_cycle2 juSetScrollItem:@[@"1.jpg",@"2.jpeg",@"3.jpg"]];
 //    ju_cycle2.juItemW=[[UIScreen mainScreen] bounds].size.width-40;
     // Do any additional setup after loading the view, typically from a nib.
 }
 -(void)viewDidAppear:(BOOL)animated{
     [super viewDidAppear:animated];
-    [ju_cycleScrollView juSetScrollItem:@[@"1.jpg",@"2.jpeg",@"3.jpg",@"4.jpeg"]];
-    [ju_cycle2 juSetScrollItem:@[@"1.jpg",@"2.jpeg",@"3.jpg"]];
+
 }
 
 - (void)didReceiveMemoryWarning {
